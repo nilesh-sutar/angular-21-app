99939249f89d644ab5d2efdade00b4293baa8791
nilesh-sutar <nilesh-sutar@users.noreply.github.com>
2026-03-05 09:19:02 +0000

    deploy: c761dd32a2e7235c2b07e7c34b44d307f03a7710

---
+++ feature/ngrx-signals/chunk-6EO37T5E.js
@@ -0,0 +1,1 @@
+import"./chunk-7CGTOI24.js";var o=[{path:"",pathMatch:"full",redirectTo:"login"},{path:"login",loadComponent:()=>import("./chunk-IKFBDS2O.js").then(t=>t.Login)},{path:"register",loadComponent:()=>import("./chunk-Q5ZQS3P7.js").then(t=>t.Register)}];export{o as authRoutes};
--- feature/ngrx-signals/chunk-XV36IEDC.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-7CGTOI24.js";var o=[{path:"",pathMatch:"full",redirectTo:"login"},{path:"login",loadComponent:()=>import("./chunk-5BWRQTKL.js").then(t=>t.Login)},{path:"register",loadComponent:()=>import("./chunk-SODDSMLV.js").then(t=>t.Register)}];export{o as authRoutes};
